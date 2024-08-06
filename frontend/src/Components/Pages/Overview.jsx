ce49b1dbf7d8ca88c38378bee375b5e634f30447
remagII <139614619+remagII@users.noreply.github.com>
2024-08-06 22:22:31 +0800

    Updated overview

--- frontend/src/Components/Pages/Overview.jsx
+++ frontend/src/Components/Pages/Overview.jsx
@@ -6,12 +6,12 @@ export default function Overview({ overviewArr }) {
     >
       <h2 className={`text-base text-gray-300 font-semibold`}>Overview</h2>
       <div className={`flex flex-col gap-1`}>
-        {overviewArr.map((item, index) => {
+        {overviewArr.map(({ title, quantity }, index) => {
           return (
             <div key={index}>
-              <h2 className={`text-xl text-white`}>{item.title}</h2>
+              <h2 className={`text-xl text-white`}>{title}</h2>
               <h1 className={`text-3xl mb-6 text-white font-semibold`}>
-                {item.quantity < 10 ? "0" + item.quantity : item.quantity}
+                {quantity < 10 ? "0" + quantity : quantity}
               </h1>
             </div>
           );
